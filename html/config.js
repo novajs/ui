1b94a25f2abf24a85541d0943600df470b25c707
Jared Allard <jaredallard@outlook.com>
2016-10-07 23:38:54 -0700

    sentry: initial setup

--- html/config.js
+++ html/config.js
@@ -4,5 +4,9 @@ window.API_CONFIG = {
   host: "https://api.tritonjs.com",
   ide: "https://ide.tritonjs.com",
   cdomain: ".tritonjs.com",
-  version: "1"
+  version: "1",
+  sentry: {
+    enabled: true,
+    SDN: "https://e54b05b10f4f4cfa83ed2fa078bdae79@sentry.tritonjs.com/1"
+  }
 }
